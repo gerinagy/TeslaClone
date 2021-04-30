f8316f71ecae4598390fc5776a8554ed6b870f05
Gergo Nagy <gergonagy@Gergos-MacBook-Pro.local>
2021-04-30 22:03:19 +0200

    Buttons

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { CarItem } from './components/CarItem';
+import StyledButton from './components/StyledButton/StyledButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <CarItem/>
-      
+      <StyledButton/>
       <StatusBar style="auto" />
     </View>
   );
