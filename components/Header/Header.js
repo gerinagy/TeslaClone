dc5001d2013a1496ac9f98e06cbd175010689910
Gergo Nagy <gergonagy@Gergos-MacBook-Pro.local>
2021-05-01 23:05:46 +0200

    Header and Flatlist

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Image } from 'react-native'
+import styles from './styles'
 
-const Header = (props) => {
+const Header = () => {
     return (
-        <View>
-            <Text>Works</Text>
+        <View style={styles.container} >
+            <Image
+                style={styles.logo}
+                source={require('../../assets/images/logo.png')} />
+            <Image
+                style={styles.menu}
+                source={require('../../assets/images/menu.png')} />
         </View>
     )
 }
 
-export default Header
+export default Header;
